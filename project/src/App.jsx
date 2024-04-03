1d416eba6099b0e13a51100d671995e7dd25a580
ValentinReese <Valentinreese88@gmail.com>
2024-04-02 22:12:32 -0700

    unfinished, i was having some errors. I will finish soon.

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
+import { puppyList } from './data.js';
+import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  
   return (
-    <>
-    
-    </>
-  )
+    <div className="App">
+         {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App
